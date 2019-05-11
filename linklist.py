d276c9efb48431f3712892bfaa4af39093703900
apoorv16 <33604782+apoorv16@users.noreply.github.com>
2019-05-11 14:37:23 +0530

    Update linklist.py

--- linklist.py
+++ linklist.py
@@ -4,47 +4,32 @@ elements to be inserted dynamically.'''
 
 
 
-
-a = []                                              #an empty stack is formed inorder to form a link list
-class node:                                         # basis of link list if formed with data and whom the data will point
-    def __init__(self,data = 0):
+                   
+class node:
+    def __init__(self,data = None):
         self.data = data
-        self.next = 0
-        self.prev = 0
-class linklist:                                     #current node tells us about the location of our present node
+        self.next = None
+class ll:
     def __init__(self):
-        self.currentnode = node()
+        self.head = None
     def insert(self,data):
-        new = node(data)                            #value of node of data is given to new and now new = 0 where new refers to new node.
-        c = self.currentnode
-        while c.next != 0:
-            c.prev = c
-            c = c.next                              #linking prev to c to next c.prev -> c -> c.next
-        c.next = new
-                                                    #the last value is new which was initially zero
-        while c.next != 0:
-            c.prev = c
-            c = c.next                              #inserting the data in a list
-            a.append(c.data)
-    def delete(self,data):
-        dell = data
-        c = self.currentnode
-        while c:                                    #loop until c does not exist, i.e triversing through the list
-            if c.data == dell:
-                a.remove(c.data)
-                c.prev = c.next                     #removing the data of c.data and then c.prev = 0 as c.prev = c.next and we know c.next=0
-            c.prev = c
-            c = c.next                              #incrementing the loop,thus c.prev , c and c.next are linked toghether due to it.
+        new = node(data)
+        if self.head == None:
+            self.head = node(data)
+        else:
+            c = self.head
+            while c.next != None:
+                c = c.next
+            c.next = new
+
     def display(self):
-        print(a)
-l = linklist()
-while(True):
-    ch = int(input("1.insert\n 2.delete\n 3.display\n"))
-    if ch == 1:
-        data = int(input())
-        l.insert(data)
-    elif ch == 2:
-        data = int(input())
-        l.delete(data)
-    elif ch == 3:
-        l.display()
+        c = self.head
+        while c.next != None:
+            print(c.data, c.next.data)
+            c = c.next
+l = ll()
+l.insert(10)
+l.insert(20)
+l.insert(30)
+l.insert(40)
+l.insert(None)
