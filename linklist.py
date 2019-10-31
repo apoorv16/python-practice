59361e2c5094429f94ca81271fce5c0454328357
apoorv16 <33604782+apoorv16@users.noreply.github.com>
2019-10-31 13:49:58 +0530

    Update linklist.py

--- linklist.py
+++ linklist.py
@@ -3,33 +3,61 @@ we have to specify how many elements we are going to insert and that many elemen
 elements to be inserted dynamically.'''
 
 
-
-                   
 class node:
-    def __init__(self,data = None):
+    def __init__(self,data):
         self.data = data
         self.next = None
-class ll:
+
+class linkedlist:
     def __init__(self):
         self.head = None
-    def insert(self,data):
-        new = node(data)
+
+    def front(self,data):
+        front_node = node(data)
+        front_node.next = self.head
+        self.head = front_node
+
+    def middle(self,data,prev_node):
+        c = self.head
+        while c.data != prev_node:
+            c = c.next
+        middle_node = node(data)
+        middle_node.next = c.next
+        c.next = middle_node
+
+    def back(self,data):
         if self.head == None:
             self.head = node(data)
         else:
-            c = self.head
-            while c.next != None:
-                c = c.next
-            c.next = new
+            self._back(self.head,data)
+    def _back(self,c,data):
+        if c.next is not None:
+            self._back(c.next,data)
+        else:
+            c.next = node(data)
 
     def display(self):
+        if self.head is None:
+            print("No Linkedlist")
+        else:
+            self._display(self.head)
+    def _display(self,c):
+        if c is not None:
+            print(c.data,c.next)
+            self._display(c.next)
+
+    def delete(self,data):
         c = self.head
-        while c.next != None:
-            print(c.data, c.next.data)
+        while c.next.data != data:
             c = c.next
-l = ll()
-l.insert(10)
-l.insert(20)
-l.insert(30)
-l.insert(40)
-l.insert(None)
+        c.next = c.next.next
+
+if __name__ == '__main__':
+    l = linkedlist()
+    l.back(10)
+    l.back(20)
+    l.back(30)
+    l.front(5)
+    l.middle(15,10)
+    l.delete(15)
+    l.display()
